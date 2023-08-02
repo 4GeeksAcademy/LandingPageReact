0ea6cb5d4ad7527b68d33f7ffd5dd97f36faf0f6
JuancarlosMuller <118220005+JuancarlosMuller@users.noreply.github.com>
2023-08-02 21:43:49 +0000

    second

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,11 +6,28 @@ import Footer from "./footer.jsx"
 
 //create your first component
 const Home = () => {
+	let imagescard = [
+		"https://picsum.photos/id/237/400/200",
+		"https://picsum.photos/id/11/400/200",
+		"https://picsum.photos/id/28/400/200",
+		"https://picsum.photos/id/280/400/200"
+	]
+
 	return (
 		<>
 			<Navbar />
 			<Container />
-			<Card />
+			<div className="container mt-3">
+				<div className="row">
+					{imagescard.length > 0 &&
+						imagescard.map((imgcrd, index) => {
+							return (
+								<Card imgcrd={imgcrd} key={index} />
+							)
+						})
+					}
+				</div>
+			</div>
 			<Footer />
 		</>
 	);
